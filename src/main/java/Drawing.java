6fb2d23fcb1e3b5e37a6ac4352d89db9bc3a6aab
Trevi03 <trevi.yek21@imperial.ac.uk>
2023-10-17 17:43:09 +0100

    update rect

--- src/main/java/Drawing.java
+++ src/main/java/Drawing.java
@@ -2,10 +2,13 @@ import java.awt.*;
 
 public class Drawing extends Canvas {
     private Circle c;
-    Drawing(int rad,Point pnt,Color col){
-        this.c = new Circle(rad,pnt,col);
+    private Rect r;
+    Drawing(int radc,Point pntc,Color colc,int wr,int hr, Point pntr, Color colr){
+        this.c = new Circle(radc,pntc,colc);
+        this.r = new Rect(wr,hr,pntr,colr);
     }
     public void paint(Graphics g){
         c.circDraw(g);
+        r.rectDraw(g);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Main {
+    public static Point p = new Point(100,75);
     static Frame frame = new Frame();
-    static Drawing drawing = new Drawing(60,new Point(100,150),Color.red);
+    static Drawing drawing = new Drawing(100,p,Color.red,20,30,p,Color.blue);
     public static void main(String[] args) {
         frame.add(drawing);
         drawing.setSize(300, 200);
